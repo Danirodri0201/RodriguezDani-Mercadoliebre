f7a0c8ece871a49d873745f7279bcfa0a88842da
Danirodri0201 <Danirodri0201>
2023-08-12 12:36:37 -0300

    creates server

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+const publicPath = path.join(__dirname, "../public");
+const static = express.static(publicPath);
+app.use(static);
+
+app.listen(3001, () => {
+  console.log("Servidor corriendo en el puerto 3001");
+});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/home.html"));
+});
